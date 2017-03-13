af957fbcf54112079f721d67db882b1e6da1ae48
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-03-13 15:50:46 +0100

    Enhance example scripts to libexec Set version to 0.3.1

--- sample_scripts/service-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# This is a sample shell script showing how you can submit the PROCESS_HOST_CHECK_RESULT
-# command to Alignak. 
-
-now=$(date +%s)
-commandfile='/tmp/alignak.cmd'
-
-printf "[$now] PROCESS_SERVICE_CHECK_RESULT;test_host_0;test_ok_0;0;Service is OK\n" > $commandfile
